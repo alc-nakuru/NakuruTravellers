29209c14e320ce2152fd0358ba0b8ad8ac488226
Timz Owen <ktimothy@kabarak.ac.ke>
2019-08-01 13:49:50 -0700

    Update ListActivity.java
    Updated the List with the Required instances from the other clases

--- main/java/com/owen/nakurutravellers/ListActivity.java
+++ main/java/com/owen/nakurutravellers/ListActivity.java
@@ -1,33 +1,97 @@
-package com.owen.nakurutravellers;
+package com.owen.travelmantics;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
 
-    ArrayList<TravelDeals> travelDealsArray;
+    ArrayList<TravelDeal> deals;
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mDatabaseReference;
-    private ChildEventListener mChildEventListener;
+    private ChildEventListener mChildListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-            RecyclerView rvDeals = (RecyclerView) findViewById(R.id.rvDeals);
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.list_activity_menu, menu);
+
+        MenuItem insertMenu = menu.findItem(R.id.insert_menu);
+
+        if (FirebaseUtil.isAdmin)
+        {
+            insertMenu.setVisible(true);
+        }
+        else
+        {
+            insertMenu.setVisible(false);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.insert_menu:
+                startActivity(new Intent(this, DealActivity.class));
+                return true;
+            case R.id.logout_menu:
+                AuthUI.getInstance()
+                        .signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                //Notify user when logout
+                                Log.d("logout", "Logged out successfuly");
+                                FirebaseUtil.attachListner();
+                            }
+                        });
+                FirebaseUtil.detachListener();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        FirebaseUtil.detachListener();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        FirebaseUtil.openfbReference("traveldeals", this);
+
+        RecyclerView rvDeals = (RecyclerView) findViewById(R.id.rvDeals);
         final  DealAdapter adapter = new DealAdapter();
         rvDeals.setAdapter(adapter);
 
@@ -35,11 +99,11 @@ public class ListActivity extends AppCompatActivity {
 
         rvDeals.setLayoutManager(dealLayoutmanager);
 
-}
-    
-     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.list_activity_menu, menu);
-        return true;
+        FirebaseUtil.attachListner();
     }
+
+    public void showMenu()
+    {
+        invalidateOptionsMenu();
+    }
+}
